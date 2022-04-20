6807e2a2daad1fa013a22a2621ed0f28f70a13b4
Gürsel Türkeri <68682737+gurselturkeri@users.noreply.github.com>
2022-04-20 16:59:00 +0300

    Update pub_icine.py

--- pub_icine.py
+++ pub_icine.py
@@ -13,85 +13,90 @@ park_yasak_levha = None
 
 def YoloFunc(data):
     
+    
 
     for box in data.bounding_boxes:
-        
+
+        global sola_donulmez_levha
+        global sola_mecburi_levha
+        global kirmizi_isik_levha
+        global yesil_isik_levha
+        global durak_levha
+        global girisi_olmayan_yol_levha
+        global saga_mecburi_levha
+        global kavsak_levha
+        global park_levha
+        global engelli_park_levha
+        global park_yasak_levha    
+
+
         if(box.Class=="sola_donulmez" and box.probability>0.80):   
-            global sola_donulmez_levha
-            sola_donulmez_levha=1     
-            print("sola_donulmez")
+            sola_donulmez_levha=True       
+        else:   
+            sola_donulmez_levha=False
 
-        if(box.Class=="sola_mecburi" and box.probability>0.80):
-            global sola_mecburi_levha
-            sola_mecburi_levha=1 
-            print("sola_mecburi")
 
-        if(box.Class=="kirmizi_isik" and box.probability>0.80):
-            global kirmizi_isik_levha
-            kirmizi_isik_levha=1 
-            print("kirmizi_isik")
+        if(box.Class=="sola_mecburi" and box.probability>0.80):  
+            sola_mecburi_levha=True
+        else:
+            sola_mecburi_levha=False       
 
-        if(box.Class=="yesil_isik" and box.probability>0.80):
-            global yesil_isik_levha
-            yesil_isik_levha=1 
-            print("yesil_isik")
+        if(box.Class=="kirmizi_isik" and box.probability>0.80):           
+            kirmizi_isik_levha=True
+        else:
+            kirmizi_isik_levha=False
 
-        if(box.Class=="durak" and box.probability>0.80):
-            global durak_levha
-            durak_levha=1 
-            print("durak")
+        if(box.Class=="yesil_isik" and box.probability>0.80): 
+            yesil_isik_levha=True
+        else:
+            yesil_isik_levha=False
+
+        if(box.Class=="durak" and box.probability>0.80): 
+            durak_levha=True
+        else:
+            durak_levha=False
 
         if(box.Class=="girisi_olmayan_yol" and box.probability>0.80):
-            global girisi_olmayan_yol_levha
-            girisi_olmayan_yol_levha=1 
-            print("girisi_olmayan_yol")
+            girisi_olmayan_yol_levha=True
+        else:
+            girisi_olmayan_yol_levha=False
 
         if(box.Class=="saga_mecburi" and box.probability>0.80):
-            global saga_mecburi_levha
-            saga_mecburi_levha=1 
-            print("saga_mecburi")
+            saga_mecburi_levha=True
+        else:
+            saga_mecburi_levha=False
 
         if(box.Class=="ada_etrafinda_donunuz" and box.probability>0.80):
-            global kavsak_levha
-            kavsak_levha=1 
-            print("ada_etrafinda_donunuz")
+            kavsak_levha=True
+        else:
+            kavsak_levha=False
 
         if(box.Class=="park" and box.probability>0.80):
-            global park_levha
-            park_levha=1 
-            print("park")
+            park_levha=True
+        else:
+            park_levha=False
 
         if(box.Class=="engelli_park" and box.probability>0.80):
-            global engelli_park_levha
-            engelli_park_levha=1 
-            print("engelli_park")
+            engelli_park_levha=True
+        else: 
+            engelli_park_levha=False
 
         if(box.Class=="park_yasak" and box.probability>0.80):
-            global park_yasak_levha
-            park_yasak_levha=1 
-            print("park_yasak")
+            park_yasak_levha=True
+        else:
+            park_yasak_levha=False
 
 
 
 
 def SolaDonulmez():
-    print("Fonksiyona girdi")
-
 def SolaMecburi():
-
 def KirmiziIsik():
-
 def YesilIsik():
-
 def Durak():
-
 def GirisiOlmayanYol():
-
 def SagaMecburi():
-
 def AdaEtrafindaDonunuz():
-
 def Park():
-
 def EngelliPark():
-def ParkYasak(): 
+def ParkYasak():
