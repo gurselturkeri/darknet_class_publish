e62b93a9fedcc4394f8a750dc27a41fdfec844a9
Gürsel Türkeri <68682737+gurselturkeri@users.noreply.github.com>
2022-04-20 16:10:57 +0300

    Update pub_icine.py

--- pub_icine.py
+++ pub_icine.py
@@ -11,53 +11,87 @@ engelli_park_levha = None
 park_yasak_levha = None
 
 
-def call_yolo(data):
+def YoloFunc(data):
     
 
     for box in data.bounding_boxes:
         
         if(box.Class=="sola_donulmez" and box.probability>0.80):   
-            global sola_donulmez_levha=1     
+            global sola_donulmez_levha
+            sola_donulmez_levha=1     
             print("sola_donulmez")
 
         if(box.Class=="sola_mecburi" and box.probability>0.80):
+            global sola_mecburi_levha
+            sola_mecburi_levha=1 
             print("sola_mecburi")
 
         if(box.Class=="kirmizi_isik" and box.probability>0.80):
+            global kirmizi_isik_levha
+            kirmizi_isik_levha=1 
             print("kirmizi_isik")
 
         if(box.Class=="yesil_isik" and box.probability>0.80):
+            global yesil_isik_levha
+            yesil_isik_levha=1 
             print("yesil_isik")
 
         if(box.Class=="durak" and box.probability>0.80):
+            global durak_levha
+            durak_levha=1 
             print("durak")
 
         if(box.Class=="girisi_olmayan_yol" and box.probability>0.80):
+            global girisi_olmayan_yol_levha
+            girisi_olmayan_yol_levha=1 
             print("girisi_olmayan_yol")
 
         if(box.Class=="saga_mecburi" and box.probability>0.80):
+            global saga_mecburi_levha
+            saga_mecburi_levha=1 
             print("saga_mecburi")
 
         if(box.Class=="ada_etrafinda_donunuz" and box.probability>0.80):
+            global kavsak_levha
+            kavsak_levha=1 
             print("ada_etrafinda_donunuz")
 
         if(box.Class=="park" and box.probability>0.80):
+            global park_levha
+            park_levha=1 
             print("park")
 
         if(box.Class=="engelli_park" and box.probability>0.80):
+            global engelli_park_levha
+            engelli_park_levha=1 
             print("engelli_park")
 
         if(box.Class=="park_yasak" and box.probability>0.80):
+            global park_yasak_levha
+            park_yasak_levha=1 
             print("park_yasak")
-           
+
+
+
+
 def SolaDonulmez():
+    print("Fonksiyona girdi")
+
 def SolaMecburi():
+
 def KirmiziIsik():
+
 def YesilIsik():
+
 def Durak():
+
 def GirisiOlmayanYol():
+
 def SagaMecburi():
+
 def AdaEtrafindaDonunuz():
+
 def Park():
+
 def EngelliPark():
-def ParkYasak():
+def ParkYasak(): 
